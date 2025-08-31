26298f599bdfcee248a5ce7a3ce7d75bdb776e95
Serge Van Meerbeeck <serge.vanmeerbeeck@gmail.com>
2025-08-31 17:20:10 +0200

    Install script - first steps

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+echo "Installing Oh My Zsh and Powerlevel10k theme..."
+if [ ! -d "$HOME/.oh-my-zsh" ] ; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Oh My Zsh is already installed"
+fi
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ] ; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+else
+  echo "powerlevel10k is already installed"
+fi
+if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/catppuchin/zsh-syntax-highlighting" ] ; then
+  git clone --depth=1 https://github.com/catppuccin/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/catppuchin/zsh-syntax-highlighting"
+else
+  echo "zsh-syntax-highlighting is already installed"
+fi
+
+echo "Installling azure-cli..."
+brew install azure-cli
+
+echo "Installing NVM..."
+brew install nvm
+
+echo "Installing Neovim..."
+brew install neovim
+
+# Aerospace installation
+# Note that when XDG_CONFIG_HOME is not set, it defaults to ~/.config, which is what the current configuration expects.
+echo "Installing Aerospace..."
+brew install --cask nikitabobko/tap/aerospace
+
+echo "Installing Yazi..."
+brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font
+
+echo "Installing Lazygit..."
+brew install lazygit
+
+echo "Installing zoxide..."
+brew install zoxide
+
+echo "Installing tmux..."
+brew install tmux
+if [ ! -d "$XDG_CONFIG_HOME/tmux/plugins/catppuccin/tmux" ] ; then
+  git clone -b v2.1.3 https://github.com/catppuccin/tmux.git $XDG_CONFIG_HOME/tmux/plugins/catppuccin/tmux
+fi
